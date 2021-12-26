ebe5d833edaef9d3961fd8ec983c350e4fc00d55
bke243 <48906220+bke243@users.noreply.github.com>
2021-12-26 20:40:06 +0100

    Setting up the intial project of the frontend applications

---
+++ admin-frontend/README.md
@@ -0,0 +1,8 @@
+# To start the project run
+### `npm start`
+
+# To run test run 
+### `npm test`
+
+# To build run 
+### `npm run build`
---
+++ user-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      Content here
+    </div>
+  );
+}
+
+export default App;
