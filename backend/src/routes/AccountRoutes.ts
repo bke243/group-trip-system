db0ad3d365128299d54dec88418d669fa18a6a4c
bke243 <48906220+bke243@users.noreply.github.com>
2022-01-16 16:18:53 +0100

    adding the admin validation on endpoints

--- backend/src/routes/AccountRoutes.ts
+++ backend/src/routes/AccountRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import AccountCallbacks from "../middleware-callbacks/AccountCallbacks";
+import AdminCallbacks from "../middleware-callbacks/AdminCallbacks";
+import isUserAuthenticated from "../middlewares/check-user-auth";
 import { validateBodyParams } from "../middlewares/request-body-validator";
 import { AccountLoginSchema, AccountSingUpSchema } from "../utils/schemas/AccountSchema.util";
 
 const router = Router();
 
 // get all accounts to be disabled and removed
-router.get("/accounts", AccountCallbacks.getAccounts);
+router.get("/accounts", isUserAuthenticated, AdminCallbacks.isAdminUser, AccountCallbacks.getAccounts);
 
 // create a user account
 router.post("/signup", validateBodyParams(AccountSingUpSchema), AccountCallbacks.hasAccountWithSameEmail, AccountCallbacks.postUserSignUp);
