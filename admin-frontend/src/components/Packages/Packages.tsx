06b0dd214ec40a25f005a6ba81b1565a182ece86
bke243 <48906220+bke243@users.noreply.github.com>
2022-01-17 17:12:52 +0100

    Adding responsiveness to the package view

--- admin-frontend/src/components/Packages/Packages.tsx
+++ admin-frontend/src/components/Packages/Packages.tsx
@@ -3,6 +3,14 @@ import { useSelector } from '../../store/react-redux-hooks';
 import Package from './integrant/Package';
 import { makeStyles } from "@mui/styles";
 import { blueButtonsColors } from '../common/colors/colors';
+import travelManImage from "../../assets/travelImage.jpg";
+import travelManImage2 from "../../assets/travelImage2.jpg";
+import travelManImage3 from "../../assets/travelImage3.jpg";
+import travelManImage4 from "../../assets/travelImage4.jpg";
+import travelManImage5 from "../../assets/travelImage5.jpg";
+import travelManImage6 from "../../assets/travelImage6.jpg";
+import travelManImage7 from "../../assets/travelImage7.jpg";
+import travelManImage8 from "../../assets/travelImage8.jpg";
 
 const useStyles = makeStyles({
   packagesContainer: {
@@ -21,6 +29,8 @@ const useStyles = makeStyles({
   },
 });
 
+const imagesList = [travelManImage, travelManImage2, travelManImage3, travelManImage4, travelManImage5, travelManImage6, travelManImage7, travelManImage8]
+
 const Packages = () => {
   const { packages } = useSelector((state) => state.package);
   const classes = useStyles();
@@ -28,10 +38,10 @@ const Packages = () => {
   return (
     <Box sx={{ flexGrow: 1, overflowX: "hidden", maxHeight: "100vh", padding: "15px" }} className={classes.packagesContainer}>
       <Grid container spacing={4} sx={{ flexGrow: 1 }} direction="row" >
-        {availablePackages.map((availablePackage) => {
+        {availablePackages.map((availablePackage, index) => {
           return ( 
-            <Grid item  key={`package-${availablePackage.id}`} xs={4} style={{ display:"flex", alignItems: "center", justifyContent: "center" }} >
-              <Package key={`package-${availablePackage.id}`} {...availablePackage} />
+            <Grid item  key={`package-${availablePackage.id}`} xs={12} md={6} lg={4} xl={3} style={{ display:"flex", alignItems: "flex-start", justifyContent: "center" }} >
+              <Package key={`package-${availablePackage.id}`} {...availablePackage} imageName={imagesList[index % imagesList.length]} />
             </Grid>
 )
         })}
